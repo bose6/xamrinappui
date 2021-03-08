564320ec69701c52ca57d22790175e4b769e73c2
bose <abhijijithbose0@gmil.com>
2021-03-08 18:02:27 +0530

    xamarin

---
+++ source/ui/ui/MainPage.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace ui
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+            var nav = new NavigationPage(new ContentPage { Title = "Page" });
+            nav.BarBackgroundColor = Color.Yellow;
+        }
+
+        private void ImageButton_Clicked(object sender, EventArgs e)
+        {
+             Navigation.PushAsync(new policy());
+
+        }
+    }
+}
---
+++ source/ui/ui/policy.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace ui
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class policy : ContentPage
+    {
+        public policy()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Clicked(object sender, EventArgs e)
+        {
+
+            
+
+        }
+
+        private void Button_back(object sender, EventArgs e)
+        {
+            Navigation.PopAsync();
+
+        }
+    }
+}
